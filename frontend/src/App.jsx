9f4d52872265995bddf8ef109c83fcca063c912b
Rahul Gautam Singh <rahultesnik@gmail.com>
2025-05-29 02:59:55 +0530

    setup(routing): add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignUpPage";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-start">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
