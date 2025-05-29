35d90fad97c2174e74d457fdc51ed06af262d598
Rahul Gautam Singh <rahultesnik@gmail.com>
2025-05-29 17:50:23 +0530

    feat(frontend): Create Problem

---
+++ frontend/src/pages/AddProblem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CreateProblemForm from "../components/CreateProblemForm";
+
+export default function AddProblem() {
+  return (
+    <div>
+      <CreateProblemForm />
+    </div>
+  );
+}
