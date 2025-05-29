ca33968498d032e5e31024509bbc355340a8036e
Rahul Gautam Singh <rahultesnik@gmail.com>
2025-05-29 17:48:06 +0530

    chore(frontend): setup zustand

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,72 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigninUp: false,
+  isLoggingIn: false,
+  isCheckingAuth: false,
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const res = await axiosInstance.get("/auth/me");
+      console.log("Check auth response: ", res.data);
+      toast.success("User authentication successful!");
+      set({ authUser: res.data.user });
+    } catch (error) {
+      console.log("Error checking auth:", error);
+      toast.error("User authentication failed!");
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigninUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/register", data);
+      console.log("Signup response: ", res.data);
+      toast.success("Signed up successfully!");
+      set({ authUser: res.data.user });
+    } catch (error) {
+      console.log("Error signing up:", error);
+      toast.error("Error signing up!");
+      set({ authUser: null });
+    } finally {
+      set({ isSigninUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      console.log("Sign in response: ", res.data);
+
+      toast.success("Logged In sucessfully!");
+      set({ authUser: res.data.user });
+    } catch (error) {
+      console.log("Error signing in:", error);
+      toast.error("Error Logging in!");
+      //   set({ authUser: null });
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      const res = await axiosInstance.post("/auth/logout");
+      console.log("Logging out response: ", res.data);
+
+      toast.success("Logged out successfully!");
+      set({ authUser: null });
+    } catch (error) {
+      console.log("Error Logging out:", error);
+      toast.error("Error Logging out!");
+    }
+  },
+}));
