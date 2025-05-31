4346c38e9a5c7d962d5ccf0d0658494adbe9b14c
Rahul Gautam Singh <rahultesnik@gmail.com>
2025-05-31 18:23:21 +0530

    feat(frontend/store): add useAction

---
+++ frontend/src/store/useAction.js
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useActionStore = create((set) => ({
+  isDeletingProblem: false,
+
+  deleteProblem: async (id) => {
+    set({ isDeletingProblem: true });
+    try {
+      const res = await axiosInstance.delete(`/problem/delete-problem/${id}`);
+      toast.success(res.data.message);
+    } catch (error) {
+      console.error("Error deleting problem", error);
+      toast.error("Error deleting problem");
+    } finally {
+      set({ isDeletingProblem: false });
+    }
+  },
+}));
