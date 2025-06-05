ca1131f4542bf748c1c3b2f8a538c972d06916b2
Rahul Gautam Singh <rahultesnik@gmail.com>
2025-06-05 22:14:16 +0530

    feat: add custom domain to cors origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,12 +11,26 @@ import playlistRoutes from "./routes/playlist.route.js";
 dotenv.config();
 console.log("Allowed origin:", process.env.FRONTEND_URL);
 console.log("PORT:", process.env.PORT);
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_HOSTED_URL,
+];
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
